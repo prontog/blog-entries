b0bac39544e1b67a5914bf07efb4f0447a6b56df
Panos Rontogiannis <p_ronto@hotmail.com>
2016-09-15 17:01:06 +0300

    Added a default level to the trace function.

--- verbosity/tool_with_advanced_verbosity.sh
+++ verbosity/tool_with_advanced_verbosity.sh
@@ -8,23 +8,37 @@ Tool that...can be variably verbose!
 
 Options:
   -h    display this help text and exit
-  -v    make the operation more talkative
+  -v    make the operation more talkative. Multiple -v options increase 
+        the verbosity.
 EOF
   exit 1
 }
 
-#### These functions can be moved to your bashrc file.
+#### These functions can be moved to your bashrc file
+# Function that handles the verbosity option.
 set_verbosity() {
-	verbosity=$((verbosity + 1))
+	verbosity_level=$((verbosity_level + 1))
 }
+# Function that echoes all passed arguments to stderr if verbosity is on. If 
+# the first parameter is numeric then the message will only be echoed if the
+# verbosity level is >= to it (the first param).
 trace() {
-	if [[ $verbosity -ge $1 ]]; then
+	local msg_level=$(($1 + 0))
+	if [[ $msg_level -gt 0 ]]; then
 		shift
-		echo $*
+	else
+		msg_level=1
+	fi
+	
+	verbosity_level=$(($verbosity_level + 0))
+	
+	if [[ $verbosity_level -ge $msg_level ]]; then
+		echo $* >&2
 	fi
 }
 ####
 
+# Handle CLI options
 while getopts "hv" option
 do
 	case $option in
@@ -32,13 +46,14 @@ do
 		h|\?) usage;;
 	esac
 done
-
 shift $(( $OPTIND - 1 ))
 
+# Start doing stuff
 echo Starting...
-trace 1 Verbosity level is $verbosity
+trace 1 Verbosity level is $verbosity_level
 trace 1 This should be verbose if verbosity level is at least 1
 trace 2 This should be verbose if verbosity level is at least 2
 trace 3 This should be verbose if verbosity level is at least 3
 trace 4 This should be verbose if verbosity level is at least 4
+trace This line has no trace level! Defaults to 1.
 echo ...finished!
